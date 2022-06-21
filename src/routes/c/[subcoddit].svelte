d897dc109bc8eefd7b51e0bd26e46b2b144fde1c
Matej Baco <matejbacocom@gmail.com>
2022-06-21 19:57:38 +0200

    Improve design, texts, responsivity

--- src/routes/c/[subcoddit].svelte
+++ src/routes/c/[subcoddit].svelte
@@ -1,80 +1,86 @@
 <script lang="ts">
-    import { goto } from '$app/navigation';
-    import { page } from '$app/stores';
-    import { AppwriteService, user, type Post, type Subcoddit } from '$lib/appwrite';
-    import { onMount } from 'svelte';
-import Id from './[subcoddit]/[id].svelte';
+	import { goto } from '$app/navigation';
+	import { page } from '$app/stores';
+	import { AppwriteService, user, type Post, type Subcoddit } from '$lib/appwrite';
+	import { onMount } from 'svelte';
+	import Id from './[subcoddit]/[id].svelte';
 
+	const subCodId: string = $page.params.subcoddit;
 
-    const subCodId:string = $page.params.subcoddit
+	let posts: Post[] = [];
+	let subCoddit: Subcoddit;
+	let urlImg: string;
+	let loading: boolean = false;
+	let curentUser: any;
 
+	function createPost() {
+		goto(subCodId + '/createPost');
+	}
 
-    let posts:Post[] = [];
-    let subCoddit:Subcoddit;
-    let urlImg:string;
-    let loading:boolean = false;
-    let curentUser:any;
-    
-    function createPost(){
-        goto(subCodId+'/createPost');
-    }
-    
-    async function getName(profId:string){
-        let profile = await AppwriteService.getProfile(profId);
-        console.log(profile.nick_name)
-        return profile.nick_name
-    }
+	async function getName(profId: string) {
+		let profile = await AppwriteService.getProfile(profId);
+		console.log(profile.nick_name);
+		return profile.nick_name;
+	}
 
-    onMount(async () => {
-        const document = await AppwriteService.getSubcoddit(subCodId);
-        const img = await AppwriteService.getSubcodditImg(document.imgId);
-        const post = await AppwriteService.getPosts(subCodId);
-        const user = await AppwriteService.getAccount()
-        curentUser = user
-        posts = post.documents; 
-        subCoddit = document;
-        urlImg = img.toString();
-        console.log(document.imgId);
-        console.log(urlImg);
-        loading = true;
-    });
+	onMount(async () => {
+		const document = await AppwriteService.getSubcoddit(subCodId);
+		const img = await AppwriteService.getSubcodditImg(document.imgId);
+		const post = await AppwriteService.getPosts(subCodId);
+		const user = await AppwriteService.getAccount();
+		curentUser = user;
+		posts = post.documents;
+		subCoddit = document;
+		urlImg = img.toString();
+		console.log(document.imgId);
+		console.log(urlImg);
+		loading = true;
+	});
 </script>
 
 {#if loading}
-<div class="jumbotron">
-    <div class="container">
-        <div class="row">
-            <div class="d-flex mt-5 justify-content-between align-items-center">
-                    <h1 class="mx-3">{subCoddit?.name}</h1>
-                
-                {#if $user !== null}
-                    <div><button type="button" class="btn btn-primary" on:click={createPost}>Add post.</button></div>
-                {/if}
-            </div>
-            <h3 class="mt-1 mx-5">{subCoddit?.description}</h3>
-        </div>
-    </div>
-</div>
+	<div class="jumbotron">
+		<div class="container">
+			<div class="row">
+				<div class="d-flex mt-5 justify-content-between align-items-center">
+					<h1 class="mx-3">{subCoddit?.name}</h1>
 
-<div class="album py-5 bg-light">
-    <div class="container">
-        <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
-            {#each posts as p (p.$id)}
-            <div class="col">
-                <div class="card shadow-sm">
-                    <div class="card-body">
-                        <p class="card-text">{p.title}</p>
-                        <div class="d-flex justify-content-between align-items-center">
-                            <div class="btn-group">
-                                <a href="/c/{subCodId}/{p.$id}"><button href="/c/{subCodId}/{p.$id}" type="button" class="btn btn-sm btn-outline-secondary">Read</button></a>
-                            </div>
-                                <small class="text-muted">@{p.profileName}</small>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            {/each}
-        </div>
-    </div>
-</div>
+					{#if $user !== null}
+						<div>
+							<button type="button" class="btn btn-primary" on:click={createPost}>Add post</button>
+						</div>
+					{/if}
+				</div>
+				<h3 class="mt-1 mx-3">{subCoddit?.description}</h3>
+			</div>
+		</div>
+	</div>
+
+	<div class="album py-5 bg-light">
+		<div class="container">
+			<div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+				{#each posts as p (p.$id)}
+					<div class="col">
+						<div class="card shadow-sm">
+							<div class="card-body">
+								<p class="card-text">{p.title}</p>
+								<div class="d-flex justify-content-between align-items-center">
+									<div class="btn-group">
+										<a href="/c/{subCodId}/{p.$id}"
+											><button
+												href="/c/{subCodId}/{p.$id}"
+												type="button"
+												class="btn btn-sm btn-outline-secondary">Read More</button
+											></a
+										>
+									</div>
+									<small class="text-muted">@{p.profileName}</small>
+								</div>
+							</div>
+						</div>
+					</div>
+				{/each}
+			</div>
+		</div>
+	</div>
 {/if}
--- src/routes/createSub.svelte
+++ src/routes/createSub.svelte
@@ -1,51 +1,77 @@
 <script lang="ts">
-import { goto } from "$app/navigation";
+	import { goto } from '$app/navigation';
 
-import { AppwriteService } from "$lib/appwrite";
+	import { AppwriteService } from '$lib/appwrite';
 
-    
-    let fileVar:any;
-    let name:string;
-    let description:string;
-    let rules:string;
-    let slug:string;
+	let fileVar: any;
+	let name: string;
+	let description: string;
+	let rules: string;
+	let slug: string;
 
-
-    async function createSub(){
-        const file = await AppwriteService.addSubcoddit(rules, description, name, fileVar[0], slug)
-        goto("/")
-    }
-
+	async function createSub() {
+		const file = await AppwriteService.addSubcoddit(rules, description, name, fileVar[0], slug);
+		goto('/');
+	}
 </script>
 
-
 <div class="mt-5">
-  <form class="needs-validation" novalidate> 
-    <div class="form-row">
-      <div class="col-md-6 mb-3">
-        <label for="validationCustom03">Name</label>
-        <input type="text" class="form-control" id="validationCustom03" placeholder="Name" required bind:value={name}>
-      </div>
-      <div class="col-md-6 mb-3">
-        <label for="validationCustom03">Slug</label>
-        <input type="text" class="form-control" id="validationCustom03" placeholder="Name" required bind:value={slug}>
-      </div>
-      <div class="col-md-6 mb-3">
-        <label for="validationCustom03">Description</label>
-        <input type="text" class="form-control" id="validationCustom03" placeholder="Description" required bind:value={description}>
-      </div>
-      <div class="form-group">
-        <label for="exampleFormControlTextarea1">Rules</label>
-        <textarea class="form-control" id="exampleFormControlTextarea1" rows="3" bind:value={rules}></textarea>
-      </div>
-      <div class="col-md-6 mb-3 mt-2">
-        <label for="validationCustom03">Cover Photo</label>
-        <div class="mt-1">
-            <input class="form-control form-control-sm" id="formFileSm" type="file" bind:files={fileVar}>
-        </div>
-      </div>
-    </div>
-    <button class="btn btn-primary" type="button" on:click={createSub}>Create subcoddit.</button>
-  </form>
+	<form class="needs-validation" novalidate>
+		<div class="form-row">
+			<div class="col-md-6 mb-3">
+				<label for="validationCustom03">Name</label>
+				<input
+					type="text"
+					class="form-control"
+					id="validationCustom03"
+					placeholder="Name"
+					required
+					bind:value={name}
+				/>
+			</div>
+			<div class="col-md-6 mb-3">
+				<label for="validationCustom03">Slug</label>
+				<input
+					type="text"
+					class="form-control"
+					id="validationCustom03"
+					placeholder="Name"
+					required
+					bind:value={slug}
+				/>
+			</div>
+			<div class="col-md-6 mb-3">
+				<label for="validationCustom03">Description</label>
+				<input
+					type="text"
+					class="form-control"
+					id="validationCustom03"
+					placeholder="Description"
+					required
+					bind:value={description}
+				/>
+			</div>
+			<div class="form-group">
+				<label for="exampleFormControlTextarea1">Rules</label>
+				<textarea
+					class="form-control"
+					id="exampleFormControlTextarea1"
+					rows="3"
+					bind:value={rules}
+				/>
+			</div>
+			<div class="col-md-6 mb-3 mt-2">
+				<label for="validationCustom03">Cover Photo</label>
+				<div class="mt-1">
+					<input
+						class="form-control form-control-sm"
+						id="formFileSm"
+						type="file"
+						bind:files={fileVar}
+					/>
+				</div>
+			</div>
+		</div>
+		<button class="btn btn-primary" type="button" on:click={createSub}>Create subcoddit</button>
+	</form>
 </div>
-  
