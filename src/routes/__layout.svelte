0350e6a3746a5fa2c209c422fe27038b1c9fb5a2
Matúš Ferčák <74977647+MatusFercak@users.noreply.github.com>
2022-06-22 13:22:50 +0200

    Test

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -2,39 +2,40 @@
 	import { goto } from '$app/navigation';
 	import { AppwriteService, user } from '$lib/appwrite';
 	import { onMount } from 'svelte';
-    let loaded:Boolean= false;
-    onMount(async () => {
-        await AppwriteService.getAccount();
-        loaded = true;
-    });
-
-    
-	
+	let loaded: Boolean = false;
+	onMount(async () => {
+		await AppwriteService.getAccount();
+		loaded = true;
+	});
 </script>
+
 <header class="bg-dark shadow-sm">
-<div class="py-3 navbar navbar-dark bg-dark shadow-sm">
-    <div class="container">
-      <a href="/" class="navbar-brand d-flex align-items-center">
-        <strong>Coddit</strong>
-      </a>
-      {#if loaded}
-      {#if $user == null}
-        <div>
-          <a href="/login"><button type="button" class="btn btn-outline-light me-2">Login</button></a>
-	      <a href="/register"><button type="button" class="btn btn-warning">Sign-up</button></a>
-        </div>	
-      {:else}
-        <a href="/user"><button type="button" class="btn btn-outline-light me-2">User</button></a>
-      {/if}
-      {/if}
-    </div>
-</div>
+	<div class="py-3 navbar navbar-dark bg-dark shadow-sm">
+		<div class="container">
+			<a href="/" class="navbar-brand d-flex align-items-center">
+				<strong>Coddit</strong>
+			</a>
+			{#if loaded}
+				{#if $user == null}
+					<div>
+						<a href="/login"
+							><button type="button" class="btn btn-outline-light me-2">Login</button></a
+						>
+						<a href="/register"><button type="button" class="btn btn-warning">Sign-up</button></a>
+					</div>
+				{:else}
+					<a href="/user"
+						><button type="button" class="btn btn-outline-light me-2">Profile</button></a
+					>
+				{/if}
+			{/if}
+		</div>
+	</div>
 </header>
 
 <div class="container">
 	<slot />
-    <div class="mt-2 d-flex justify-content-center">
-        <p class="mt-1 mb-3 text-muted justify-content-center">©Coddit 2022–2023</p>
-    </div>
-	
+	<div class="mt-2 d-flex justify-content-center">
+		<p class="mt-1 mb-3 text-muted justify-content-center">©Coddit 2022–2023</p>
+	</div>
 </div>
